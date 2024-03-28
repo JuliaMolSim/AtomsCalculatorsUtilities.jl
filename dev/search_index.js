5641a51dd29de1b1cb65958f8d888dc5c04b76b7
Documenter.jl <documenter@juliadocs.github.io>
2024-03-28 23:18:03 +0000

    build based on fc01ac7

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = AtomsUtilityCalculators","category":"page"},{"location":"#AtomsUtilityCalculators","page":"Home","title":"AtomsUtilityCalculators","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for AtomsUtilityCalculators.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [AtomsUtilityCalculators]","category":"page"}]
+}
